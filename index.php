116485cbd40e9f8d42afa868436947a982d874d6
Misir Jafarov <misir.ceferov@gmail.com>
2018-10-20 13:09:16 +0400

    Added __DIR__ before file paths

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php include 'backend/session.php';?>
-<?php include 'common/header.php'; ?>
+<?php include __DIR__.'/backend/session.php';?>
+<?php include __DIR__.'/common/header.php'; ?>
 <title>My Personal CV</title>
-<?php include 'common/body.php';?>
+<?php include __DIR__.'/common/body.php';?>
 <div class="user-dashboard">
 	<iframe src="resume_page.php" width="100%" height="1000px"></iframe>
 </div>
-<?php include 'common/footer.php';?>
+<?php include __DIR__.'/common/footer.php';?>
